e015f38ac0d6b0157f4964251b7aa3d2fa7e1221
Pontus Karlsson <43955663+PGamingHD@users.noreply.github.com>
2023-10-16 13:30:53 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,9 @@ namespace AddressList
                 else if (command.ToLower() == "stop") {
                     
                 }
+                else if (command.ToLower() == "load") {
+
+                }
                 else {
                     Console.WriteLine($"Unknown Command: {command}");
                 }
